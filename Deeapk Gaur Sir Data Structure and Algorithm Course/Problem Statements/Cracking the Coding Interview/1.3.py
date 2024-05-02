ce74946767c09a098f32d36210eefb949c64d76c
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2024-05-02 18:42:30 +0530

    1.3 of cracking the coding interview

---
+++ Deeapk Gaur Sir Data Structure and Algorithm Course/Problem Statements/Cracking the Coding Interview/1.3.py
@@ -0,0 +1,11 @@
+str1 = input()
+str1.strip()
+for i in range(len(str1)):
+    if(str1[i]!=' '):
+        print(str1[i],end='')
+    else:
+        print("%20",end='')
+        
+        
+
+        
