796b873a840244cad9128a6d4333cddeeed7190a
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2024-05-29 23:58:29 +0530

    Lecture 14 Done!

---
+++ PW Skills/Lecture 15/matrix_multiplication.cpp
@@ -0,0 +1,27 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr1[2][2]= {1,2,3,4};
+    int arr2[2][2] = {1,2,3,4};
+    for(int i=0; i<2; i++)
+    {
+        for(int j =0; j<2; j++)
+        {
+            
+            for(int k= 0; k<2; k++)
+            {
+                
+               
+            }
+        }
+    }
+    for(int i=0; i<2; i++)
+    {
+        for(int j =0; j<2; j++)
+        {
+            cout<<arr1[i][j]<<' ';
+        }
+        cout<<endl;
+    }
+}
