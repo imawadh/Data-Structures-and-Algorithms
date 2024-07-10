a3d914db4aafeacb6c83ca5308ca0ab0cc049ac9
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2024-07-11 00:06:07 +0530

    Day 1 with DSA in JavaScript

---
+++ Vishwa Mohan Sir in JavaScript/Codes/Basic/script1.js
@@ -0,0 +1,17 @@
+// function print(n){
+//     console.log("Hello world!");
+// }
+
+// print(100);
+// print(1000);
+// print(10000);
+
+// Here No. of operation doesn't varies with input size , so it is called as O(1);
+
+function print (n){
+    for(let i=0; i<n; i++){
+        console.log("Hello world!");
+    }
+}
+
+print(7);
---
+++ Vishwa Mohan Sir in JavaScript/Codes/Basic/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function print(n){
+//     console.log("Hello world!");
+// }
+
+// print(100);
+// print(1000);
+// print(10000);
